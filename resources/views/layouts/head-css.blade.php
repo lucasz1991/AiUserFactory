1cd8d35597f49742b9a9c107ec195dc7c61e5dbb
Lucas Zacharias <lucas@zacharias-net.de>
2026-06-30 05:45:23 +0200

    dfdfdf

--- resources/views/layouts/head-css.blade.php
+++ resources/views/layouts/head-css.blade.php
@@ -2,5 +2,6 @@
 <link rel="stylesheet" href="{{ URL::asset('build/css/icons.min.css') }}" />
 <link rel="stylesheet" href="{{ URL::asset('build/css/tailwind.min.css') }}" />
 <link rel="stylesheet" href="{{ URL::asset('build/libs/apexcharts/apexcharts.css') }}" />
+<link rel="stylesheet" href="{{ asset('adminresources/fontawesome6/css/all.min.css') }}">
 
 
